6532d0642976ce6283cc8b564862548183357441
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-13 21:42:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(number)
   start = 2 
   if number > 1 
-    range - (start..number-1).to_a 
+    range = (start..number-1).to_a 
     range.none? do |num| 
       number % num == 0 
     end 
